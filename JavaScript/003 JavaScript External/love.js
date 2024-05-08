270c16a45e6185f3cfc3c8fd4ef7feb4325a5af3
Yugandhar Gampala <129308425+yugandhar0407@users.noreply.github.com>
2024-05-08 14:43:47 +0530

    Add files via upload

---
+++ JavaScript/003 JavaScript External/love.js
@@ -0,0 +1,5 @@
+function love(){
+                                                     // We Can  Write JavaScript Code using .js Extension 
+    document.write(" hello I Love You Too");
+    alert("I Love You ");
+}
